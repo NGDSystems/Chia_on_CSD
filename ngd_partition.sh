933eacf8848147c1e4c2df4e11023d0ccdbd6a2b
Hossein <31414094+Bobarshad@users.noreply.github.com>
2022-02-04 15:19:16 -0800

    Add files via upload

---
+++ ngd_partition.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ $# -eq 2 ]; then 
+m=$1
+n=$2 
+for ((i=$m; i<=$n; i++ ))
+do
+echo " -------- nvme$i -------";
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | sudo fdisk "/dev/nvme"$i"n1"
+    g # new partition    
+    n # new partition
+    1 # partition number 1
+      # default - start at beginning of disk 
+      # 1T data parttion
+    w # write the partition table
+EOF
+sudo mkfs.ext4 "/dev/nvme"$i"n1p1";
+done
+else
+  echo "Please use './1-par.sh [m] [n]' # m for first nvme drive and n for last nvme drive. Example: './1-par.sh 0 2'"; 
+fi
