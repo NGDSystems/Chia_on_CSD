3e2c88d31f68bace012a808099dfd8b1a2dbb162
Hossein <31414094+Bobarshad@users.noreply.github.com>
2022-02-04 15:24:21 -0800

    Add files via upload

---
+++ kill_ploters.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -eq 3 ]; then 
+  m=$1
+  n=$2
+  j=$3
+	for ((i=$m; i<=$n; i++ ))  
+	do 
+		echo ------$i-----; 
+		pid=`ps aux | grep chia | grep newport_$j/tmp$i| awk '{print $2}'`;
+		#ps aux | grep chia | grep newport_$i/tmp | wc -l;
+		sudo kill $pid;
+		echo $pid;
+        rm -r /mnt/newport_$j/tmp$i
+	done
+else
+  echo "Please use './kill_plots.sh [m] [n]' # m for first nvme drive and n for last nvme drive. Example: './kill_plots.sh 0 2'";
+fi
